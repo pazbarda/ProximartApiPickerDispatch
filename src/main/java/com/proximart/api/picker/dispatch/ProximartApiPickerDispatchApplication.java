b729a0ccc118bd9517f7f2f0f5c57f0c00fc063e
pazbarda <pazbarda@gmail.com>
2020-11-10 21:28:01 +0200

    initial commit (vanilla mvn project)

---
+++ src/main/java/com/proximart/api/picker/dispatch/ProximartApiPickerDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.proximart.api.picker.dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximartApiPickerDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximartApiPickerDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proximart/api/picker/dispatch/ProximartApiPickerDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proximart.api.picker.dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximartApiPickerDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
